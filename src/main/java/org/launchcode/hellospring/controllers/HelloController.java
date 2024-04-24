ed4765ddcd6a59b25a68de236218342e7a2368ff
i-swa <ievaswanson@u.boisestate.edu>
2024-04-23 19:39:53 -0500

    SpringBoot practice. Created Controller class, two methods, and two routes

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,20 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    // Handles request at path /hello
+    @GetMapping("hello")
+    @ResponseBody
+    public String Hello() {
+        return "Hello, Spring!";
+    }
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String Goodbye() {
+        return "Goodbye, Spring!";
+    }
+}
