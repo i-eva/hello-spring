ae62a7914c59e7c09e4ffce3b92e3d42c0c36be7
i-swa <ievaswanson@u.boisestate.edu>
2024-04-23 19:47:28 -0500

    Created query parameter

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,19 +2,27 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     // Handles request at path /hello
-    @GetMapping("hello")
-    @ResponseBody
-    public String Hello() {
-        return "Hello, Spring!";
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String Hello() {
+//        return "Hello, Spring!";
+//    }
     @GetMapping("goodbye")
     @ResponseBody
     public String Goodbye() {
         return "Goodbye, Spring!";
+    }
+
+    // Responds to get requests at /hello?name=LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 }
